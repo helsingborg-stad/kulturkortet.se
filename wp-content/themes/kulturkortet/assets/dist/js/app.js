f9ef94107f44b464b5551237370b0caf47ff5523
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2018-05-18 11:23:39 +0200

    test gulp

---
+++ wp-content/themes/kulturkortet/assets/dist/js/app.js
@@ -0,0 +1,27 @@
+var Kulturkortet;
+
+Kulturkortet = Kulturkortet || {};
+Kulturkortet.ExampleNamespace = Kulturkortet.Liquid || {};
+
+Kulturkortet.ExampleNamespace.ExampleClass = (function ($) {
+
+	var classVariable = false;
+
+    /**
+     * Constructor
+     * Should be named as the class itself
+     */
+	function ExampleClass() {
+
+    }
+
+    /**
+     * Method
+     */
+    ExampleClass.prototype.exampleMethod = function () {
+
+    }
+
+	return new ExampleClass();
+
+})(jQuery);
