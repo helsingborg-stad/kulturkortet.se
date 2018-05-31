322bc6b0cab91277a188fe4d558356260eaf5d1e
Sebastian Thulin <sebastian.thulin@gmail.com>
2018-05-31 15:21:41 +0200

    Fixes faulty translation loader.

--- wp-content/themes/kulturkortet/library/Entity/Taxonomy.php
+++ wp-content/themes/kulturkortet/library/Entity/Taxonomy.php
@@ -26,14 +26,14 @@ class Taxonomy
         $labels = array(
             'name'              => $this->namePlural,
             'singular_name'     => $this->nameSingular,
-            'search_items'      => sprintf(__('Search %s', 'todo'), $this->namePlural),
-            'all_items'         => sprintf(__('All %s', 'todo'), $this->namePlural),
-            'parent_item'       => sprintf(__('Parent %s:', 'todo'), $this->nameSingular),
-            'parent_item_colon' => sprintf(__('Parent %s:', 'todo'), $this->nameSingular) . ':',
-            'edit_item'         => sprintf(__('Edit %s', 'todo'), $this->nameSingular),
-            'update_item'       => sprintf(__('Update %s', 'todo'), $this->nameSingular),
-            'add_new_item'      => sprintf(__('Add New %s', 'todo'), $this->nameSingular),
-            'new_item_name'     => sprintf(__('New %s Name', 'todo'), $this->nameSingular),
+            'search_items'      => sprintf(__('Search %s', 'kulturkortet'), $this->namePlural),
+            'all_items'         => sprintf(__('All %s', 'kulturkortet'), $this->namePlural),
+            'parent_item'       => sprintf(__('Parent %s:', 'kulturkortet'), $this->nameSingular),
+            'parent_item_colon' => sprintf(__('Parent %s:', 'kulturkortet'), $this->nameSingular) . ':',
+            'edit_item'         => sprintf(__('Edit %s', 'kulturkortet'), $this->nameSingular),
+            'update_item'       => sprintf(__('Update %s', 'kulturkortet'), $this->nameSingular),
+            'add_new_item'      => sprintf(__('Add New %s', 'kulturkortet'), $this->nameSingular),
+            'new_item_name'     => sprintf(__('New %s Name', 'kulturkortet'), $this->nameSingular),
             'menu_name'         => $this->nameSingular,
         );
 
