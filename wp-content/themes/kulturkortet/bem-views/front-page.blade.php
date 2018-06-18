23c3baad5603f17d58fc93ddcd541acc483ef3c1
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2018-06-19 00:03:40 +0200

    override frontpage template (to remove creamy etc)

---
+++ wp-content/themes/kulturkortet/bem-views/front-page.blade.php
@@ -0,0 +1,20 @@
+@extends('templates.master')
+
+@section('layout')
+
+@if (is_active_sidebar('content-area'))
+    <section class="u-py-5 sidebar-content-area">
+        <div class="container">
+            @include('components.dynamic-sidebar', ['id' => 'content-area'])
+        </div>
+    </section>
+@endif
+
+@if (is_active_sidebar('content-area-bottom'))
+    <div class="container u-py-5 sidebar-content-area-bottom">
+        <div class="grid grid--columns">
+                @include('components.dynamic-sidebar', ['id' => 'content-area-bottom'])
+        </div>
+    </div>
+@endif
+@stop
