8dcee1f5d91990ef314c7f4c08f7ad909077322c
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2018-07-04 14:23:07 +0200

    Show date if post type article

--- wp-content/themes/kulturkortet/bem-views/partials/article.blade.php
+++ wp-content/themes/kulturkortet/bem-views/partials/article.blade.php
@@ -2,10 +2,15 @@
 <article class="c-article s-article">
     <h1>{{ the_title() }}</h1>
 
-    @include('partials.accessibility-menu')
+    @if (get_post_type() == 'article')
+        <div class="u-mb-1">
+            <time datetime="<?php echo the_time('Y-m-d H:i'); ?>">
+                    <strong> <?php the_time('j F Y'); ?></strong>
+            </time>
+        </div>
+    @endif
 
     @if (isset(get_extended($post->post_content)['main']) && strlen(get_extended($post->post_content)['main']) > 0 && isset(get_extended($post->post_content)['extended']) && strlen(get_extended($post->post_content)['extended']) > 0)
-
         {!! apply_filters('the_lead', get_extended($post->post_content)['main']) !!}
         {!! apply_filters('the_content', get_extended($post->post_content)['extended']) !!}
 
@@ -15,7 +20,5 @@
         @else
         {!! the_content() !!}
         @endif
-
     @endif
-
 </article>
