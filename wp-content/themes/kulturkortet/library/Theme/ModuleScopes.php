ad7bbb6d98110a0eac6b6c4901572604b27e67ec
Jonatan Hanson <jonatan.hanson@hiq.se>
2018-07-18 14:32:27 +0200

    Added custom option view for selected events

--- wp-content/themes/kulturkortet/library/Theme/ModuleScopes.php
+++ wp-content/themes/kulturkortet/library/Theme/ModuleScopes.php
@@ -7,6 +7,7 @@ class ModuleScopes
     public function __construct()
     {
         add_filter('Modularity/Editor/ModuleCssScope', array($this, 'addModuleScopes'));
+        add_filter('acf/load_field/key=field_571dfd4c0d9d9', array($this, 'addPostTemplates'));
     }
 
     /**
@@ -24,5 +25,16 @@ class ModuleScopes
         );
 
         return $scopes;
+    }
+
+    /**
+     * Add custom view values
+     * @param $field
+     * @return mixed
+     */
+    public function addPostTemplates($field)
+    {
+        $field['choices']['selected-events'] = __('Selected events', 'kulturkortet');
+        return $field;
     }
 }
